4d8ed6c01a2617b30ace4e18febd796d3d7f5ca4
CuB1z <cub1zdev@gmail.com>
2024-10-21 20:40:52 +0200

    chore: Add longlines function implementation (to be reviewed)

--- libreria.c
+++ libreria.c
@@ -1,5 +1,6 @@
 #include "libreria.h"
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 const int LINE_LENGTH = 1024;
@@ -10,13 +11,13 @@ const int LINE_LENGTH = 1024;
  * @return 0 if the function was executed successfully.
  */
 int head(int N) {
+    size_t len = LINE_LENGTH;
     char * buffer;
+    int i;
 
     // Allocate memory for the buffer
-    size_t len = LINE_LENGTH;
     buffer = (char *) malloc(len);
 
-    int i;
     for (i = 0; i < N; i++) {
         // Read a line from stdin
         int readBytes = getline(&buffer, &len, stdin);
@@ -50,6 +51,65 @@ int tail(int N) {
  * @return 0 if the function was executed successfully.
  */
 int longlines(int N) {
-    printf("Executed longlines %d\n", N);
+    size_t len = LINE_LENGTH;
+    char ** data;
+    char * buffer;
+    int count = 0;
+    int i, worstLine, worstLineIndex;
+
+    // Allocate memory for the data storage
+    data = (char **) malloc(N * sizeof(char *));
+    for (i = 0; i < N; i++) {
+        data[i] = (char *) malloc(len);
+    }
+
+    // Allocate memory for the buffer
+    buffer = (char *) malloc(len);
+
+    // Iterate over the stdin
+    while (getline(&buffer, &len, stdin) != EOF) {
+        // Get current line length
+        int lineLength = strlen(buffer);
+
+        printf("Buffer: %s\n", buffer);
+        printf("Size: %d\n\n", lineLength);
+        
+        // Add the line if the data storage isn't full yet
+        if (count < N) {
+            printf("Buffer: %s", buffer);
+            strcpy(data[count], buffer);
+            count++;
+        
+        // Check if the new line is better than the worst line
+        } else {
+            // Initialize "worst" variables
+            worstLine = strlen(data[0]);
+            worstLineIndex = 0;
+
+            // Iterate over the rest of the data and update "worst" variables
+            for (i = 1; i < N; i++) {
+                if (strlen(data[i]) < worstLine) {
+                    worstLine = strlen(data[i]);
+                    worstLineIndex = i;
+                }
+            }
+
+            // Check if the current line is better than the worst one
+            if (lineLength > worstLine) {
+                strcpy(data[worstLineIndex], buffer);
+            }
+        }
+    }
+
+    // Print data and free lines allocated memory
+    for (i = 0; i < N; i++) {
+        printf("%s", data[i]);
+        free(data[i]);
+    }
+
+    // Free previous allocated memory
+    free(data);
+    free(buffer);
+
     return 0;
 }
